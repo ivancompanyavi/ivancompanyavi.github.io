d2da9b30969b40f52b8149a3ea17c5f01f985323
Ivan Company <ivancompanyavi@gmail.com>
2021-02-23 20:11:48 -0800

    First commit

---
+++ content/pages/about.rst
@@ -0,0 +1,4 @@
+About
+#####
+
+My name is Ivan Company and I've been working as a software developer since 2008. During all these years I've worked on different languages, libraries and teams, playing different roles on the different companies. Because of this, I thought it could be a good idea to share some of my thoughts during this time, providing a more human vision of what development is and why it's so fun :)
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Ivan Company'
+SITENAME = 'Brain Bucket'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Vancouver'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('Linkedin', 'https://www.linkedin.com/in/ivancompany/'),
+          ('Twitter', 'https://twitter.com/theIvanCompany'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = "iris"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
