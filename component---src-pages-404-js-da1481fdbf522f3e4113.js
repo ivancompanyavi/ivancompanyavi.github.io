79bf29f5be718db18fbb645e8060ba82f97db917
Ivan Company <ivancompanyavi@gmail.com>
2021-10-07 19:54:39 -0700

    Updates

---
+++ component---src-pages-404-js-da1481fdbf522f3e4113.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkBrain_Bucket=self.webpackChunkBrain_Bucket||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),u=a(7198),l=a(3751);e.default=function(t){var e=t.data,a=t.location,i=e.site.siteMetadata.title;return n.createElement(u.Z,{location:a,title:i},n.createElement(l.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-da1481fdbf522f3e4113.js.map
